29a9ca33f876f41752b6edc6d43c47daec902490
aa-ag <aaron.aguerrevere@gmail.com>
2020-10-14 17:00:48 -0500

    links

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-result = requests.get("https://google.com/")
+# result = requests.get("https://google.com/")
+result = requests.get("https://www.whitehouse.gov/briefings-statements/")
+# input = input("Enter a url: ")
+# result = requests.get(input)
 
 # print(result.status_code)
 # print(result.headers)
@@ -11,10 +14,17 @@ src = result.content
 
 soup = BeautifulSoup(src, 'lxml')
 
-links = soup.find_all("a")
+# links = soup.find_all("a")
 # print(links)
 
-for link in links:
-    if "About" in link.text:
-        print(link)
-        print(link.attrs['href'])
+# for link in links:
+#     if "About" in link.text:
+#         print(link)
+#         print(link.attrs['href'])
+
+urls = []
+for h2_tag in soup.find_all('h2'):
+    a_tag = h2_tag.find('a')
+    urls.append(a_tag.attrs['href'])
+
+print(urls)
