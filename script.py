beb1d3ad1b16cb80caecd7c8bb94e688da280509
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-29 21:39:49 -0500

    Add soup that calls on every image

--- script.py
+++ script.py
@@ -1,10 +1,16 @@
 ############------------ IMPORTS ------------##################################
 import requests
+from bs4 import BeautifulSoup
 
 ############------------ FUNCTION(S) ------------##############################
 def scrape_images(url):
     r = requests.get(url)
-    print(r.status_code)
+    
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    for img in soup.select("img"):
+        print(img.get("src"))
+
 
 ############------------ DRIVER CODE ------------##############################ß
 if __name__ == "__main__":
