e2a40b98559c1ad687d5c13ae726c4a6952b88fc
aa-ag <aaron.aguerrevere@gmail.com>
2022-08-29 21:35:35 -0500

    Rearrange logic

--- script.py
+++ script.py
@@ -1,30 +1,12 @@
+############------------ IMPORTS ------------##################################
 import requests
-from bs4 import BeautifulSoup
 
-# result = requests.get("https://google.com/")
-# result = requests.get("https://www.whitehouse.gov/briefings-statements/")
-input = input("Enter a url: ")
-result = requests.get(input)
+############------------ FUNCTION(S) ------------##############################
+def scrape_images(url):
+    r = requests.get(url)
+    print(r.status_code)
 
-# print(result.status_code)
-# print(result.headers)
-
-src = result.content
-# print(src)
-
-soup = BeautifulSoup(src, 'lxml')
-
-# links = soup.find_all("a")
-# print(links)
-
-# for link in links:
-#     if "About" in link.text:
-#         print(link)
-#         print(link.attrs['href'])
-
-urls = []
-for h2_tag in soup.find_all('h2'):
-    a_tag = h2_tag.find('a')
-    urls.append(a_tag.attrs['href'])
-
-print(urls[0])
+############------------ DRIVER CODE ------------##############################ß
+if __name__ == "__main__":
+    url = "https://support.apple.com/mac/macbook-pro"
+    scrape_images(url)
